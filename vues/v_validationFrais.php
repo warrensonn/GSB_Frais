f1f2a589ffbc1256fd37570727fc584332980da2
Warren <bevilacqua.warren@gmail.com>
2021-04-25 23:02:17 +0200

    Display two list, one of fichefrais's visitors having at least one 'Fiche créée, saisie en cours' fichefrais and one of fichefrais's months having at least one 'Fiche créée, saisie en cours' fichefrais. The accountant has the option to ask for the display of the selected fichefrais's informations

---
+++ vues/v_validationFrais.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Vue État de Frais des fiches validées
+ *
+ * PHP Version 7
+ * 
+ * L'utilisateur comptable à la possibilité de valider la mise en paiement des fiches de frais validées
+ *
+ * @category  PPE
+ * @package   GSB
+ * @author    Warren BEVILACQUA <bevilacqua.warren@gmail.com
+ * @version   GIT: <0>
+ * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
+ */
+?>
+	<form class="s12" action="index.php?uc=validerFrais&action=voirEtatFrais" method="post">
+		<h2>Les fiches de frais</h2>
+			
+		<div class="input-field col s8">
+			<label>Visiteur :</label>
+			<select id="lstVis" name="lstVis">
+				<?php
+				if(!empty($lesVisiteurs)){ ?>
+					<?php
+					foreach ($lesVisiteurs as $unVisiteur)
+					{					
+						?>
+							<option value="<?php echo $unVisiteur['id'] ?>" <?php if($idAutreVisiteur == $unVisiteur['id']) { ?> selected <?php } ?> ><?php echo $unVisiteur['nom']." ".$unVisiteur['prenom']; ?></option>
+						<?php 
+					}
+				}else{
+				?>
+					<option><?php echo 'Aucun Visiteur ne possède de fiche de frais'; ?></option>
+				<?php
+				}
+				?>
+			</select>
+			
+		</div>
+		
+		<div class="input-field col s8">
+			<label>Mois :</label>
+			<select id="lstMois" name="lstMois">
+				<?php
+				foreach ($lesMois as $unMois)
+				{
+					?>
+						<option value="<?php echo $unMois['mois'] ?>" <?php if($leMois == $unMois['mois']) { ?> selected <?php } ?> ><?php echo $unMois['numMois']."/".$unMois['numAnnee']; ?></option>
+					<?php
+				}
+				?>
+			</select>
+		</div>
+		
+		<div class="" class="row col s12">
+			<button class="btn btn-info btn-lg" type="submit" name="valider">Search
+				<i class="glyphicon glyphicon-search"></i>
+			</button>
+		</div>
+	</form>
+<br><br>
+
+<?php
+if (!empty($messageValidationOk)) { ?>
+<div class="panel panel-info"> 
+        <div class="panel-heading"> <?php echo $messageValidationOk; ?>
+</div> <?php
+} ?>
