2093e64473aa536792bc0e34edb487e1ac45cf6b
Warren <bevilacqua.warren@gmail.com>
2021-04-25 18:29:31 +0200

    updating parameters of the 'creeNouveauFraisHorsForfait' function which has changed in the 'fct.inc.php' file

--- controleurs/c_gererFrais.php
+++ controleurs/c_gererFrais.php
@@ -8,6 +8,7 @@
  * @package   GSB
  * @author    Réseau CERTA <contact@reseaucerta.org>
  * @author    José GIL <jgil@ac-nice.fr>
+ * @author    BEVILACQUA Warren <bevilacqua.warren@gmail.com
  * @copyright 2017 Réseau CERTA
  * @license   Réseau CERTA
  * @version   GIT: <0>
@@ -47,7 +48,8 @@ case 'validerCreationFrais':
             $mois,
             $libelle,
             $dateFrais,
-            $montant
+            $montant,
+            'ACC'
         );
     }
     break;
